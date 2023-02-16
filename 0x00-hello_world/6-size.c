64997419923a081a04cf00a1136a0e88fca2502f
Gordon <gordiefx@gmail.com>
2023-02-16 12:13:45 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a char: %u byte(s)\n", sizeof(a));
 	printf("Size of an int: %u byte(s)\n", sizeof(b));
 	printf("Size of a long int: %u byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %u byte(s)\n");
+	printf("Size of a long long int: %u byte(s)\n", sizeof(d));
 	printf("Size of a float: %u byte(s)\n", sizeof(e));
 	return (0);
 }
