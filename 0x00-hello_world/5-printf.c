e9e8b5bcbb50d65b5c51da1fe3db04897191b425
Gordon <gordiefx@gmail.com>
2023-02-16 11:04:31 -0800

    pritf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - output using printf
- * Return 0
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
