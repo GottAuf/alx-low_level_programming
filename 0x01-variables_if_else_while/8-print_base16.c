c54887ca176010b115bbc288f508b7b6cca90289
Gordon <gordiefx@gmail.com>
2023-02-17 11:35:42 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - hexa
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+		putchar(48 + x);
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
+	putchar('\n');
+	return(0);
+}
