a3446532db791a748c9718e45caef7e3ed005def
Gordon <gordiefx@gmail.com>
2023-02-21 15:25:48 -0800

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 
 /**
  * _islower - check lower case
+ *@c: character to check
  *
- * Return: 1 if c is lowercase
- * 	   0 otherwise
+ * Return: 1 if c is lowercase 0 otherwise
  */
 int _islower(int c)
 {
