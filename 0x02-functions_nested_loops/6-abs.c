d1fbcbd3cea55824de050db66d45e1bed0f98ca5
Gordon <gordiefx@gmail.com>
2023-02-21 16:17:12 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @p: number to be absolute
+ *
+ * Return: absolute
+ */
+int _abs(int p)
+{
+	if (p<0)
+	{
+		p*=-1;
+	}
+	return (p);
+}
