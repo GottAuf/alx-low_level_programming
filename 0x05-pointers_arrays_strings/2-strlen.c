2cfb2df50bd9644473cdf997fd5fe08278fa91bc
Gordon <gordiefx@gmail.com>
2023-02-28 10:27:35 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to be checked
+ *
+ * Return: length of string s as int type
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+	for (; *s != '\0', s++)
+	{
+		a++;
+	}
+	return (a);
+}
