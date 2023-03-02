db394285567f61a8acb3f8d61fe632d90eacd34c
Gordon <gordiefx@gmail.com>
2023-03-02 12:01:29 -0800

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @str: string to modify
+ *
+ * Return: char
+ * */
+char *string_toupper(char *str)
+{
+	int a = 0;
+
+	while (str[a])
+	{
+		if (str[a] >= 97 && str[a] <= 122)
+		{
+			str[a] -= 32;
+		}
+		a++;
+	}
+	return (str);
+}
