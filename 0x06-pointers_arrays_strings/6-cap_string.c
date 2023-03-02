5c9e20909e4375f2eddfa91cc0c8eca3e1e80b3a
Gordon <gordiefx@gmail.com>
2023-03-02 12:29:36 -0800

    cpitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words
+ * @str: string to modify
+ *
+ * Return: char str
+ */
+char *cap_string(char *str)
+{
+	int a = 0; 
+	int b;
+	char chr[] = {32, 44, 46, '\t', '\n', ';', '!', '(', ')', '?', '"', '{', '}'};
+
+	while (str[a])
+	{
+		b =0;
+
+		while (b < 13)
+		{
+			if ((a == 0 || str[a - 1] == chr[b]) && str[a] >= 97 && str[a] <= 122))
+				str[a] -= 32;
+			
+			b++;
+		}
+
+		a++;
+	}
+	return (str);
+}
